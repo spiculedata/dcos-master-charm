ec28d42525c17dfef8545fc058a55a2501b1bb13
Tom Barber <tom@analytical-labs.com>
2016-04-25 10:44:59 +0100

    update for release

--- README.md
+++ README.md
@@ -1,3 +1,47 @@
-# dcos-master-charm
-# dcos-master-charm
-# dcos-master-charm
+# Overview
+
+DC/OS is the Data Center Operating System from Mesosphere. It is built upon Apache MESOS and is designed to turn your data center into a single pool of resources that applications can utilise.
+
+Mesosphere released DC/OS open source edition to run on a number of cloud computing services and also support for Centos, this charm leverages that capability to install DC/OS inside Ubuntu using the Juju modelling framework.
+
+Juju and DC/OS make an excellent fit because it allows administrators to add new resource as simply as adding more DC/OS Agent units and deploying more applications.
+
+# Usage
+
+## Warning
+
+**This charm is currently under heavy development and has limited testing outside of EC2**
+
+Step by step instructions on using the charm:
+
+    juju deploy dcos-master
+
+Currently the dashboard is hosted on the nodes internal IP address for EC2 and supported services. The easiest way to gain access is to SSH to the node and forward port 80 to localhost:
+
+    sudo ssh ubuntu@52.51.37.150 -L 80:localhost:80
+
+## Scale out Usage
+
+**Currently not implemented**
+DC/OS provides a master and agent setup. For fault tolerance it is recommended to run more than 1 master node, these are self configuring and distributing.
+
+## Known Limitations and Issues
+
+This charm is an initial prototype to ensure that the main services bootstrap and applications can be deployed, there will be a number of missing features and usability issues. To help get in touch! (info@spicule.co.uk)
+
+# Configuration
+
+Currently there are no configuration options.
+
+# Contact Information
+
+Tom Barber - info@spicule.co.uk
+
+## DC/OS master charm for Juju
+
+  - http://spicule.co.uk/dcos
+  - https://github.com/buggtb/dcos-master-charm/issues
+
+
+[service]: http://example.com
+[icon guidelines]: https://jujucharms.com/docs/stable/authors-charm-icon
--- reactive/dcos_master.py
+++ reactive/dcos_master.py
@@ -1,30 +1,47 @@
 import os
-import stat
-from shutil import rmtree
 from charms.reactive import when, when_not, set_state
 from subprocess import check_call, CalledProcessError, call, check_output, Popen
 from charmhelpers.core.host import adduser, chownr, mkdir
 from charmhelpers.fetch.archiveurl import ArchiveUrlFetchHandler
 from charmhelpers.core import hookenv
-from charmhelpers.core.hookenv import unit_private_ip
-from charmhelpers.core.hookenv import log
-import contextlib
-import lzma
-import tarfile
+from charmhelpers.core.hookenv import unit_private_ip, status_set, log
 
 basedir="/opt/mesosphere/"
 configdir="/etc/mesosphere/"
+au = ArchiveUrlFetchHandler()
+ip = unit_private_ip()
 
 @when_not('dcos-master.installed')
 def install_dcosmaster():
-    log("starting hook")
-    os.symlink("/bin/mkdir", "/usr/bin/mkdir")
+    status_set('maintenance', 'Installing DC/OS Master')
+    createFolders()
+    createInitFiles()
+    createSymlinks()
+    log("fetching bootstrap")
+    setupEnvVars()
+    log("remove dnsmasq")
+    dnsprocess = check_output(["apt-get", "remove", '-y',"--auto-remove", "dnsmasq-base"])
+    log("install")
+    process = check_output(["./pkgpanda", "setup"], cwd=basedir+"bin", env=setupEnvVars())
+    log("open ports")
+    hookenv.open_port(80)
+    hookenv.open_port(8181)
+    set_state('dcos-master.installed')
+
+
+def createFolders():
+    status_set('maintenance', 'Creating DC/OS Folders')
     mkdir(configdir)
     mkdir(configdir+'roles')
     mkdir(basedir)
     mkdir(configdir+'setup-flags')
     mkdir(basedir+'packages/dcos-config--setup_b3e41695178e35239659186b92f25820c610f961')
     mkdir(basedir+'packages/dcos-metadata--setup_b3e41695178e35239659186b92f25820c610f961')
+    mkdir('/etc/profile.d')
+    mkdir('/etc/systemd/journald.conf.d')
+
+def createInitFiles():
+    status_set('maintenance', 'Creating DC/OS folders')
     open(configdir+'roles/master', 'a').close()
     open(configdir+'setup-flags/repository-url', 'a').close()
     os.chmod(configdir+'setup-flags/repository-url', 0o644)
@@ -36,16 +53,24 @@ def install_dcosmaster():
     text_file.write("[\"dcos-config--setup_b3e41695178e35239659186b92f25820c610f961\", \"dcos-metadata--setup_b3e41695178e35239659186b92f25820c610f961\"]")
     text_file.close()
     os.chmod(configdir+'setup-flags/cluster-packages.json', 0o644)
-    mkdir('/etc/systemd/journald.conf.d')
     text_file = open("/etc/systemd/journald.conf.d/dcos.conf", "w")
     text_file.writelines(["[Journal]", "MaxLevelConsole=warning"])
     text_file.close()
     os.chmod('/etc/systemd/journald.conf.d/dcos.conf', 0o644)
-    mkdir('/etc/profile.d')
-    au = ArchiveUrlFetchHandler()
-    log("fetching bootstrap")
+
+def createSymlinks():
+    status_set('maintenance', 'Creating DC/OS symlinks')
+    #Hack to make start scripts work
+    os.symlink("/bin/mkdir", "/usr/bin/mkdir")
+
+def downloadBootstrap():
+    status_set('maintenance', 'Downloading bootstrap tarball')
     au.download("http://community.meteorite.bi/tmp/bootstrap.tar.gz", "/tmp/bootstrap.tar.gz")
+    #We unzip using tar on the command line because its much quicker than using python.
     check_output(['tar', 'xvfz', "/tmp/bootstrap.tar.gz", '-C', basedir])
+
+def setupEnvVars():
+    status_set('maintenance', 'Configuring the install environment variables')
     currentenv = dict(os.environ)
     currentenv['LD_LIBRARY_PATH'] = basedir+"lib"
     currentenv['PATH'] = basedir+"bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin"
@@ -58,20 +83,13 @@ def install_dcosmaster():
     currentenv['DCOS_VERSION'] = "1.7-open"
     currentenv['PROVIDER'] = "onprem"
     currentenv['MESOS_IP_DISCOVERY_COMMAND'] = basedir+"bin/detect_ip"
-    log("get private ip")
-    ip = unit_private_ip()
+    return currentenv
+
+def setupMasterConfigs():
+    status_set('maintenance', 'Creating master configs')
     text_file=open(basedir+"packages/dcos-config--setup_b3e41695178e35239659186b92f25820c610f961/etc/exhibitor", 'w')
     text_file.writelines(["EXHIBITOR_BACKEND=STATIC","EXHIBITOR_STATICENSEMBLE=1:"+ip])
     text_file.close()
     text_file=open(basedir+"packages/dcos-config--setup_b3e41695178e35239659186b92f25820c610f961/etc/master_list", 'w')
     text_file.write("[\""+ip+"\"]")
     text_file.close()
-    log("remove dnsmasq")
-    dnsprocess = check_output(["apt-get", "remove", '-y',"--auto-remove", "dnsmasq-base"])
-    log("install")
-    process = check_output(["./pkgpanda", "setup"], cwd=basedir+"bin", env=currentenv)
-    log("open ports")
-    hookenv.open_port(80)
-    hookenv.open_port(8181)
-    set_state('dcos-master.installed')
-
