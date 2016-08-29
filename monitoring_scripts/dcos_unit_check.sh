587c4d7e81f04705bf01023317575d1f1a0797f4
Tom Barber <tom@analytical-labs.com>
2016-08-29 17:10:54 +0200

    add nagios scripts

---
+++ monitoring_scripts/dcos_unit_check.sh
@@ -0,0 +1,11 @@
+# cat dcos_unit_check.sh
+#!/bin/bash
+for unit in `ls /etc/systemd/system/dcos.target.wants`; do
+  echo "Checking $unit"
+  ./check_service.sh -o linux -s ${unit} > /dev/null 2>&1
+  STATUS=$?
+  if [ "${STATUS}" -ne 0 ]; then
+    echo "Status for $unit is not 0, got $STATUS"
+    exit $STATUS
+  fi
+done
